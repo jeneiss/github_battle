10e7b019f3c45498d1edd43004ecf3900aa75b4d
montresor34 <8246292+montresor34@users.noreply.github.com>
2020-12-09 10:54:24 +0000

    Add onClick func to nav buttons

--- app/components/Popular.js
+++ app/components/Popular.js
@@ -1,6 +1,20 @@
 import React from 'react'
 
 export default class Popular extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      selectedLanguage: 'All'
+    }
+
+    this.updateLanguage = this.updateLanguage.bind(this)
+  }
+
+  updateLanguage(selectedLanguage) {
+    this.setState({selectedLanguage})
+  }
+
   render() {
     const languages = ['All', 'JavaScript', 'Ruby', 'Java', 'CSS', 'Python']
 
@@ -9,7 +23,14 @@ export default class Popular extends React.Component {
         {languages.map((lang) => {
           return (
             <li key={lang}>
-              <button className='nav__btn'>
+              <button
+                className='nav__btn'
+                style={lang === this.state.selectedLanguage ? {
+                  backgroundColor: 'red',
+                  color: 'white'
+                } : null}
+                onClick={() => this.updateLanguage(lang)}
+              >
                 {lang}
               </button>
             </li>
