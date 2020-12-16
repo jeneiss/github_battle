f753f61105c1a6931d2ea3ba6754c904311a8651
montresor34 <8246292+montresor34@users.noreply.github.com>
2020-12-16 12:27:48 +0000

    Update Battle component with conditional PlayerInput rendering

--- app/components/Battle.js
+++ app/components/Battle.js
@@ -94,10 +94,47 @@ PlayerInput.proprTypes = {
 }
 
 export default class Battle extends React.Component {
+  constructor(props) {
+    super(props)
+
+    this.state = {
+      playerOne: null,
+      playerTwo: null
+    }
+
+    this.handleSubmit = this.handleSubmit.bind(this)
+  }
+
+  handleSubmit(id, player) {
+    this.setState({
+      [id]: player
+    })
+  }
+
   render() {
+    const { playerOne, playerTwo } = this.state
+
     return (
       <React.Fragment>
         <Instructions />
+
+        <div className='players__container'>
+          <h1 className='players__header'>Players</h1>
+          <div className='players__inputs-container'>
+            {!playerOne &&
+              <PlayerInput
+                label='Player One'
+                onSubmit={(player) => this.handleSubmit('playerOne', player)}
+              />
+            }
+            {!playerTwo &&
+              <PlayerInput
+                label='Player Two'
+                onSubmit={(player) => this.handleSubmit('playerTwo', player)}
+              />
+            }
+          </div>
+        </div>
 
       </React.Fragment>
     )
