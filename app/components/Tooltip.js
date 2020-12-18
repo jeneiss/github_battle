f3eaf6af436df2ffb60800007a4abceb3eb3be1a
montresor34 <8246292+montresor34@users.noreply.github.com>
2020-12-18 12:55:23 +0000

    Create and implement withHover and Tooltip components

---
+++ app/components/Tooltip.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import withHover from './withHover'
+
+const styles = {
+  container: {
+    position: 'relative',
+    display: 'flex'
+  },
+  tooltip: {
+    boxSizing: 'border-box',
+    position: 'absolute',
+    width: '10rem',
+    bottom: '100%',
+    left: '50%',
+    marginLeft: '-5rem',
+    borderRadius: '3px',
+    backgroundColor: 'hsla(0, 0%, 20%, 0.9)',
+    padding: '0.5rem',
+    marginBottom: '0.3125rem',
+    color: '#fff',
+    textAlign: 'center',
+    fontSize: '1rem'
+  }
+}
+
+function Tooltip ({ text, children, hovering }) {
+  return (
+    <div style={styles.container} >
+      {hovering && <div style={styles.tooltip}>{text}</div>}
+      {children}
+    </div>
+  )
+}
+
+Tooltip.propTypes = {
+  text: PropTypes.string.isRequired,
+  hovering: PropTypes.bool.isRequired
+}
+
+export default withHover(Tooltip, 'hovering')
